6f21e89fd618a0bca3fe7a87fe56b6f2f9adaf2d
YutoHayashi <y_hayashi@alta.co.jp>
2022-04-08 15:53:26 +0900

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [
+        './app/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        fontFamily: {
+            'sans': [ 'Hiragino Sans', ],
+            'notosans': [ 'Noto Sans JP', ],
+            'futura': [ 'Futura Std', ],
+        },
+        extend: {
+            fontSize: Object.assign(
+                { base: '62.5%', },
+                ...Array.from( Array( 50 ) ).map( ( _, i ) => ( { [ i ]: `${ ( i * .1 ).toPrecision( 2 ) }rem` } ) )
+            ),
+        },
+    },
+    plugins: [
+        require( '@tailwindcss/forms' ),
+    ],
+}
