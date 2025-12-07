7d5c2679a9d793258b73fdc0992237e8d49684e8
YutoHayashi <yutohayashi@hotmail.com>
2025-12-07 22:43:31 +0900

    Upgrade remix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,20 @@ module.exports = {
     ],
     theme: {
         fontFamily: {
-            'sans': [ 'Hiragino Sans', ],
-            'notosans': [ 'Noto Sans JP', ],
-            'futura': [ 'Futura Std', ],
+            'sans': ['Hiragino Sans'],
+            'notosans': ['Noto Sans JP'],
+            'futura': ['Futura Std'],
         },
         extend: {
             fontSize: Object.assign(
-                { base: '62.5%', },
-                ...Array.from( Array( 50 ) ).map( ( _, i ) => ( { [ i ]: `${ ( i * .1 ).toPrecision( 2 ) }rem` } ) )
+                {
+                    base: '62.5%',
+                },
+                ...Array.from(Array(50)).map((_, i) => ({ [i]: `${(i * .1).toPrecision(2)}rem` }))
             ),
         },
     },
     plugins: [
-        require( '@tailwindcss/forms' ),
+        require('@tailwindcss/forms'),
     ],
 }
